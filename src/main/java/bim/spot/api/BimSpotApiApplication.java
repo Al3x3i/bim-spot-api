ee492533f57b3c01f12294544d6344cdf0f9f0ba
Aleksei Shashin <aleksei.shashin@gmail.com>
2020-11-08 11:39:04 +0100

    Init: Added basic Spring Boot files

---
+++ src/main/java/bim/spot/api/BimSpotApiApplication.java
@@ -0,0 +1,13 @@
+package bim.spot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimSpotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimSpotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bim/spot/api/BimSpotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bim.spot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimSpotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
