7f2303da3cfc296e86b55efb5ed06c9c1e074c92
Aleksei Shashin <aleksei.shashin@gmail.com>
2020-11-08 22:25:11 +0100

    Added Api documentation
    - Swagger 2

--- src/main/java/bim/spot/api/ApiController.java
+++ src/main/java/bim/spot/api/ApiController.java
@@ -3,20 +3,26 @@ package bim.spot.api;
 import bim.spot.api.icu.IcuService;
 import bim.spot.api.icu.SpeciesCategoryEnum;
 import bim.spot.api.icu.SpeciesClassNameEnum;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@Api(tags = "BIM SPOT API")
 public class ApiController {
 
     @Autowired
     private IcuService icuService;
 
-    @GetMapping("preview")
+    @GetMapping(value = "preview", produces = {APPLICATION_JSON_VALUE})
+    @ApiOperation("Returns list of Species filtered by `Critically Endangered`")
     public ResponseEntity preview(@RequestParam(value = "region", required = false) String region) {
         SpeciesResponse response;
         if (Strings.isNotBlank(region)) {
